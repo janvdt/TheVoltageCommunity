8e5c5b85c6697e34d770defaf6aa6fbd2efd8a0a
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-04-15 17:26:43 +0200

    soundcloud
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/post/music/index.blade.php
+++ app/views/post/music/index.blade.php
@@ -1,7 +1,13 @@
 @extends('instance.layout')
 
 @section('instanceContent')
-
+<script src="//connect.soundcloud.com/sdk.js"></script>
+<script>
+  SC.initialize({
+    client_id: "706bb7625906c6e65ff8bb1bebdd22b7",
+    redirect_uri: "http://example.com/callback.html",
+  });
+</script>
 <div class="span12">
 	<div class="span11">
 	<a href="{{ URL::action('PostController@editMusic', array($post->id)) }}" class="btn btn-primary pull-right">Edit post</a>
@@ -14,20 +20,11 @@
 		<img class="avatar img-polaroid" src="/{{ $post->image->getSize('medium')->getPathname() }}" alt="">
 	</div>
 	<div class="span7">
-		<div class="postsoundcloud">
-		<?php
-			require_once 'Services/Soundcloud.php';
-			// create a client object with your app credentials
-			$client = new Services_Soundcloud('b4479c54bc012db248c6bf606545d409', '3938d333342c58f6d5fd9febef26e7ed');
-			$client->setCurlOptions(array(CURLOPT_FOLLOWLOCATION => 1));
-
-			// get a tracks oembed data
-			$track_url = $post->soundcloud;
-			$embed_info = json_decode($client->get('oembed', array('url' => $track_url)));
-	
-			// render the html for the player widget
-			print $embed_info->html;
-			?>
+		<div id="postsoundcloud">
+		<div id="putTheWidgetHere"></div>
+		<script type="text/JavaScript">
+  			SC.oEmbed("{{$post->soundcloud}}", {color: "ff0066"},  document.getElementById("postsoundcloud"));
+		</script>
 		</div>
 		<div>
 		<p>{{$post->body}}</p>
