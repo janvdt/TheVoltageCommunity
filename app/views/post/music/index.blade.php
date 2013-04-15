ae1603b02d901a8d8ce6bc9b5b6e3b4f02e484af
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-04-15 19:14:23 +0200

    permission edit post
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/post/music/index.blade.php
+++ app/views/post/music/index.blade.php
@@ -9,7 +9,9 @@
 </script>
 <div class="span12">
 	<div class="span11">
+	@if(Auth::user()->id == $post->created_by)
 	<a href="{{ URL::action('PostController@editMusic', array($post->id)) }}" class="btn btn-primary pull-right">Edit post</a>
+	@endif
 	<h4>{{$post->title}}</h4>
 	</div>
 </div>
