18f57c4db614fccac375a4c8c5bae94c7b28c4aa
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-04-15 17:10:13 +0200

    migrations
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

---
+++ app/database/migrations/2013_04_09_115146_create_musicposts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusicpostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function($table)
+		{
+			$table->increments('id');
+			$table->string('title', 255);
+			$table->text('body');
+			$table->integer('image_id');
+			$table->string('type');
+			$table->text('soundcloud');
+			$table->integer('created_by');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
