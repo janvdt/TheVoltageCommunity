4989b280ac26c07b7428b553f2d184a8892951a1
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-05-13 17:53:24 +0200

    sd
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/instance/header.blade.php
+++ app/views/instance/header.blade.php
@@ -3,7 +3,89 @@
 		<a href="{{{ URL::to('/') }}}"><img class="" src="/images/logovoltage.png" alt=""></a>
 	</div>
 
-	
+	@if (Auth::check() or Session::has('hybridAuth'))
+	<ul class="nav span6">
+		
+			<li class="dropdown pull-right">
+				<a id="choose-instance" href="" role="button" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i>
+				@if(Auth::user())
+				 Welcome {{Auth::user()->first_name}} {{Auth::user()->last_name}}
+				@else
+				Welcome {{$facebookuser->first_name}} {{$facebookuser->last_name}}
+				@endif
+				<b class="caret"></b></a>
+
+				<ul class="dropdown-menu" role="menu" aria-labelledby="choose-instance">
+					<li>
+						@if(Auth::user())
+						<a href="{{ URL::action('UserController@showAccount',array(Auth::user()->id)) }}"><i class="icon-eye-open"> View Account</i></a>
+						@else
+						<a href="{{ URL::action('UserController@showAccount',array($facebookuser->id)) }}"><i class="icon-eye-open"> View Account</i></a>
+						@endif
+
+					</li>
+					<li><a href=""><i class="icon-key"> Change password</i></a></li>
+					<li><a href="{{ URL::action('PostController@create') }}"><i class="icon-plus"> Create post</i></a></li>
+				</ul>
+			</li>
+		
+	</ul>
+	@endif
+	@if(Auth::user() or Session::has('hybridAuth'))
+	<ul class="nav span1 pull-right">
+		
+			<li class="dropdown pull-right">
+				<a id="choose-instance" href="" role="button" class="dropdown-toggle" data-toggle="dropdown"><span class="badge badge-important">{{count($notcount)}}</span><b class="caret"></b></a>
+
+				<ul class="dropdown-menu notifications span4" role="menu">
+					@foreach($notifications as $notification)
+						@if(Auth::user() and $notification->post->created_by == Auth::user()->id or $notification->post->created_by == $facebookuser->id)
+							@if($notification->activity == FALSE)
+							@if($notification->viewed == FALSE)
+								<li class="notificationsitem span3">
+									<div class="alert alert-success">
+									<a class="" href="{{URL::action('PostController@showMusic',array($notification->post_id)) }}">
+  									 {{$notification->user->first_name}} {{$notification->body}}
+  									 @if($notification->post->soundcloud_art != NULL)
+											<img class="img-rounded pull-right" src="{{ url($notification->post->soundcloud_art) }}" alt="" width="25">
+										@else
+											<img class="img-rounded pull-right" src="{{ url($notification->post->youtube_art) }}" alt="" width="25">
+										@endif
+									</a>
+									</div>
+								</li>
+							@else
+								<li class="notificationsitem span3">
+									<div class="alert">
+									<a class="" href="{{URL::action('PostController@showMusic',array($notification->post_id)) }}">
+  									 {{$notification->user->first_name}} {{$notification->body}}
+  									 @if($notification->post->soundcloud_art != NULL)
+											<img class="img-rounded pull-right" src="{{ url($notification->post->soundcloud_art) }}" alt="" width="25">
+										@else
+											<img class="img-rounded pull-right" src="{{ url($notification->post->youtube_art) }}" alt="" width="25">
+										@endif
+									</a>
+									</div>
+									
+									
+								</li>
+							@endif
+							@endif
+						@endif
+					@endforeach
+					@foreach($following as $follow)
+						<li class="notificationsitem span3">
+							{{$follow->account->user->notification}}
+						
+						</li>
+					@endforeach
+				</ul>
+			</li>
+		</ul>
+	<div class="span2 pull-right">
+		<a href="{{ URL::action('HomeController@showActivity') }}"><i class="icon-bullhorn"> Activity log</i></a>
+	</div>
+	@endif
 </div>
 
 <div id="top-navbar" class="navbar navbar-static-top navbar-inverse span12">
