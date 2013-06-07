f855c690eeb325d6ea3522a24d672423584a2190
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-06-07 15:49:17 +0200

    qsdqsd
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -528,7 +528,7 @@ $("#soundcloud").select2({
  	$("#suggestions").show();
  	if(searchVal !== '') {
  
-            $.get('http://thevoltagecommunity.com/search?searchData='+searchVal, function(returnData) {
+            $.get('http://tvc.loc/search?searchData='+searchVal, function(returnData) {
                 /* If the returnData is empty then display message to user
                  * else our returned data results in the table.  */
                 if (!returnData) {
