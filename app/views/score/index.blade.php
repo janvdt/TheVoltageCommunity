98660d921fe0d0fb52ec165e900aae15f09ca23a
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-06-18 14:48:30 +0200

    sqdqsd
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/score/index.blade.php
+++ app/views/score/index.blade.php
@@ -6,21 +6,7 @@
 <ul class="nav">
 @foreach($totalscores as $totalscore)
 <li>
-	<div class="span12 score">
-		<div class="span1">
-			<p class="totalscore">{{$totalscore->value}}</p>
-		</div>
-		<div class="span1">
-			
-		</div>
-
-		<div class="span4">
-			
-		</div>
-		<div class="span3">
-			<img class="levelimg" src="/images/{{$totalscore->account->levels->first()->image}}" width="50">
-		</div>
-	</div>
+	{{$totalscore->value}}
 </li>
 @endforeach
 
