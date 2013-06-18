994c8ee3b2240f41991d3c5dc83e831693fb5f6f
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-06-18 14:52:24 +0200

    qsdqsd
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/score/index.blade.php
+++ app/views/score/index.blade.php
@@ -6,7 +6,27 @@
 <ul class="nav">
 @foreach($totalscores as $totalscore)
 <li>
-	{{$totalscore->value}}
+	<div class="span12 score">
+		<div class="span1">
+			<p class="totalscore">{{$totalscore->value}}</p>
+		</div>
+		<div class="span1">
+			<a href="{{ URL::action('UserController@visitAccount',array($totalscore->account->user->id)) }}">
+				@if($totalscore->account->user->identifier != 0)
+				<img class="img-polaroid imgscore" src="{{ url($totalscore->account->facebookpic) }}" alt="" width="75px">
+				@else
+				<img class="img-polaroid imgscore" src="{{ url($totalscore->account->getImagePathname()) }}" alt="" width="75px">
+				@endif
+			</a>
+		</div>
+
+		<div class="span4">
+			<p class="email">{{$totalscore->account->user->first_name}} {{$totalscore->account->user->last_name}}</p>
+		</div>
+		<div class="span3">
+			<img class="levelimg" src="/images/{{$totalscore->account->levels->first()->image}}" width="50">
+		</div>
+	</div>
 </li>
 @endforeach
 
