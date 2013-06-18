7766f1a531d7c379ad0aa07fdd8c884dfddc530f
Jan Van der Taelen <janvandertaelen@gmail.com>
2013-06-18 14:43:35 +0200

    qsdqsd
    Signed-off-by: Jan Van der Taelen <janvandertaelen@gmail.com>

--- app/views/score/index.blade.php
+++ app/views/score/index.blade.php
@@ -10,7 +10,18 @@
 		<div class="span1">
 			<p class="totalscore">{{$totalscore->value}}</p>
 		</div>
-		
+		<div class="span1">
+			<a href="{{ URL::action('UserController@visitAccount',array($totalscore->account->user->id)) }}">
+				
+			</a>
+		</div>
+
+		<div class="span4">
+			<p class="email">{{$totalscore->account->user->first_name}} {{$totalscore->account->user->last_name}}</p>
+		</div>
+		<div class="span3">
+			<img class="levelimg" src="/images/{{$totalscore->account->levels->first()->image}}" width="50">
+		</div>
 	</div>
 </li>
 @endforeach
